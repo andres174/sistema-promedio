5ae694691f1c0691d054b7894d1da6df4c805747
andres174 <zambrasda17@gmail.com>
2023-02-17 22:53:23 -0500

    form array

--- src/app/components/promedios/promedios.component.ts
+++ src/app/components/promedios/promedios.component.ts
@@ -1,5 +1,6 @@
 import { EstudiantesInterface, ExamenInterface, NotasInterface, PromedioInterface } from './../../interfaces/promedios';
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-promedios',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./promedios.component.css']
 })
 export class PromediosComponent implements OnInit {
+
+  public formPadre: FormGroup = new FormGroup({});
+
+  numEstudiantes!: number;
+  nE: number[] = [];
 
   inEstudiantes:boolean = false;
   inNotas: boolean = false;
@@ -18,9 +24,39 @@ export class PromediosComponent implements OnInit {
   examen: ExamenInterface[] = [];
   promedio: PromedioInterface[] = [];
 
+  listaEstudiante:string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  initFormPadre(){
+    this.formPadre = new FormGroup({
+      est: new FormArray([], [Validators.required])
+    });
+  }
+
+  initFormEstudiantes(): FormGroup {
+    return new FormGroup({
+      nombre: new FormControl('', [Validators.required])
+    });
+  }
+
+  addEstudiante(): void {
+    const est = this.formPadre.get('est') as FormArray;
+    est.push(this.initFormEstudiantes());
+  }
+
+  crear(){
+    for (let index = 0; index < this.numEstudiantes; index++) {
+      this.nE.push(index+1);
+    }
+    console.log(this.nE);
+  }
+
+  ver(){
+    console.log(this.listaEstudiante);
   }
 
 }
