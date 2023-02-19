c51756f9e1eb095f75b114afb2d1ce737a19befd
andres174 <zambrasda17@gmail.com>
2023-02-19 16:50:31 -0500

    promedio avance

--- src/app/components/pages/promedio/promedio.component.ts
+++ src/app/components/pages/promedio/promedio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EstudiantesService } from 'src/app/services/estudiantes.service';
 
 @Component({
   selector: 'app-promedio',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromedioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiEstudiantes: EstudiantesService) { }
+
+  estudiantesNota:any[] = [];
+
+  estudiantesProm:any[] = [];
+
+  isAdd:boolean = false;
 
   ngOnInit(): void {
+    this.getEstudiantesConNota();
+  }
+
+
+  getEstudiantesConNota(){
+    this.apiEstudiantes.getEstudiantesConNota().subscribe({
+      next: (res) => {
+        this.estudiantesNota = res;
+        console.log(res);
+      }
+    });
+  }
+
+  calcularPromedios(){
+    
+  }
+
+  addNota(id:number){
+
   }
 
 }
